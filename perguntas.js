e77e376dae4172bb0f505c56e1178684a3b3d8b6
ValdirValentino <elioelianaenrique59@gmail.com>
2024-11-08 11:21:13 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que tem dentro das minhas calças?',
+    'DARK SOULS 2'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais meus pronomes?',
+    'DARK / SOULS2'
+)
+
+criaCartao(
+    'Programação',
+    'Qual o meu nome?',
+    'DARK SOULS 2'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Do que eu não gosto?',
+    'DARK SOULS 3'
+)
